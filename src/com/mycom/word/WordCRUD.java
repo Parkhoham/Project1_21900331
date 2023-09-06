2c77d56db345a9b812085b94e917a06ebcb4dd73
Parkhoham <hoham01@handong.ac.kr>
2023-09-06 22:39:50 +0900

    This is wordmanager.

--- src/com/mycom/word/WordCRUD.java
+++ src/com/mycom/word/WordCRUD.java
@@ -1,13 +1,36 @@
 package com.mycom.word;
 
-public class WordCRUD implements ICRUD{
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class WordCRUD implements ICRUD{
+	ArrayList<Word> list;
+	Scanner s;
+	
+	WordCRUD(Scanner s){
+		list = new ArrayList<>();
+		this.s = s;
+	}
+	
 	@Override
 	public Object add() {
-		// TODO Auto-generated method stub
-		return null;
+		System.out.println();
+		System.out.print("=> 난이도(1,2,3) & 새 단어 입력 : ");
+		int level = s.nextInt();
+		String word = s.nextLine();
+		
+		System.out.print("뜻 입력: "); 
+		String meaning = s.nextLine();
+		return new Word(0, level, word, meaning);
 	}
-
+	
+	public void addWord() {
+		Word one = (Word)add();
+		list.add(one);
+		System.out.println("새 단어가 단어장에 추가되었습니다 !!!");
+		System.out.println();
+		
+	}
 	@Override
 	public int update(Object obj) {
 		// TODO Auto-generated method stub
@@ -24,6 +47,15 @@ public class WordCRUD implements ICRUD{
 	public void selectOne(int id) {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	public void listAll() {
+		System.out.println("------------------------------------");
+		for(int i=0;i<list.size();i++) {
+			System.out.print((i+1)+" ");
+			System.out.println(list.get(i).toString());
+		}
+		System.out.println("------------------------------------");
 	}
 
 }
